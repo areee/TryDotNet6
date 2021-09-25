abfd815258785aaf490919e255e2fce147822963
areee <10089872+areee@users.noreply.github.com>
2021-09-25 22:53:32 +0300

    Add also changes made into Program.cs

--- TryDotNet6/Program.cs
+++ TryDotNet6/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TryDotNet6;
+
+// A source of inspiration: https://twitter.com/okyrylchuk/status/1441749328943411202/photo/1
+
+DateOnly testingASpecificDate = new(2021, 09, 25);
+
+Console.WriteLine($"Current date: {testingASpecificDate}");
+
+DateTime today = DateTime.Now;
+
+DateOnly dateAfterTwoYears = DateOnly.FromDateTime(today.AddYears(2));
+
+Console.WriteLine($"Two years from now: {dateAfterTwoYears}");
+
+// Another source of inspiration: https://twitter.com/okyrylchuk/status/1441084950518849536/photo/1
+
+List<Animal> animals = new();
+
+Animal dog1 = new() { Name = "Musti", Breed = "Finnish Lapphund" };
+animals.Add(dog1);
+
+Animal dog2 = dog1 with { Name = "Rekku", Species = IAnimal.SpeciesEnum.Dog };
+animals.Add(dog2);
+
+Animal dog3 = dog1 with { Breed = "Karelian Beer Dog" }; // ;)
+animals.Add(dog3);
+
+foreach (var animal in animals)
+{
+    Console.WriteLine($"Name of the dog: {animal.Name}; breed of the dog: {animal.Breed}; species of the dog: {animal.Species}");
+}
 
