aa01bd08859bde2d6a630aa042110b45f79efbbe
areee <10089872+areee@users.noreply.github.com>
2021-09-25 22:53:02 +0300

    Add the first version of this trial console app

---
+++ TryDotNet6/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace TryDotNet6
+{
+    public struct Animal : IAnimal
+    {
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        public IAnimal.SpeciesEnum Species { get; set; }
+    }
+}
+
---
+++ TryDotNet6/IAnimal.cs
@@ -0,0 +1,19 @@
+﻿namespace TryDotNet6
+{
+    public interface IAnimal
+    {
+        enum SpeciesEnum
+        {
+            NotSet = 0,
+            Dog = 1,
+            Cat = 2
+        }
+
+        string Name { get; set; }
+
+        string Breed { get; set; }
+
+        SpeciesEnum Species { get; set; }
+    }
+}
+
